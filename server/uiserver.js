dd3bd41f0c5162d3c21be92b23bf70874360106c
qiangfang <mjfun82@163.com>
2020-07-24 20:34:26 -0400

    Changes for Proxy Mode

--- server/uiserver.js
+++ server/uiserver.js
@@ -40,8 +40,8 @@ app.use(express.static('public'));
 
 const apiProxyTarget = process.env.API_PROXY_TARGET;
 if (apiProxyTarget) {
-  app.use('/graphql', proxy({ target: apiProxyTarget }));
-  app.use('/auth', proxy({ target: apiProxyTarget }));
+  app.use('/graphql', proxy({ target: apiProxyTarget, changeOrigin: true }));
+  app.use('/auth', proxy({ target: apiProxyTarget, changeOrigin: true }));
 }
 
 // const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
